0d988e23f37b1d02c96fc39899d1aa2e282ebb24
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-20 16:17:56 -0500

    Add gameboard array and getGameboard function
    This commit adds the gameboard array and the getGameboard function to
    the createGameboard factory function. It also exports getGameboard and
    initiates the app assinging createGameboard to gameboard const to be
    able to preview in the browser.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const createGameboard = function () {};
+const createGameboard = function () {
+  let boardPositions = [
+    [["top-left"], ["top-center"], ["top-right"]], // top row
+    [["mid-left"], ["mid-center"], ["mid-right"]], // middle row
+    [["bot-left"], ["bot-center"], ["bot-right"]], // bottom row
+  ];
+
+  const getGameboard = () => boardPositions;
+
+  return { getGameboard };
+};
 
 const createPlayer = function () {};
 
 const createGameflow = function () {};
+
+const gameboard = createGameboard();

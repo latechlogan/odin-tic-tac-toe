c289f98f1a59bdcc1a2145eff751023378d0f3d7
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-20 16:53:22 -0500

    Refactor gameboard array and reassign it to reset

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const createGameboard = function () {
   let boardPositions = [
-    [["top-left"], ["top-center"], ["top-right"]], // top row
-    [["mid-left"], ["mid-center"], ["mid-right"]], // middle row
-    [["bot-left"], ["bot-center"], ["bot-right"]], // bottom row
+    ["top-left", "top-center", "top-right"], // top row
+    ["mid-left", "mid-center", "mid-right"], // middle row
+    ["bot-left", "bot-center", "bot-right"], // bottom row
   ];
 
   const getGameboard = () => boardPositions;
 
-  return { getGameboard };
+  const resetGameboard = () => {
+    boardPositions = [
+      [null, null, null], // top row
+      [null, null, null], // middle row
+      [null, null, null], // bottom row
+    ];
+  };
+
+  return { getGameboard, resetGameboard };
 };
 
 const createPlayer = function () {};
 
 const createGameflow = function () {};
 
+// intialize for testing/previewing in browser
 const gameboard = createGameboard();
+
+gameboard.resetGameboard();
+console.log(gameboard.getGameboard());

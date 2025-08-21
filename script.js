26feb37117d48ddf3b3f7a0f0dfb1476bf01cb26
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-08-20 20:26:19 -0500

    Add evaluation to gameboard to identify winner

--- script.js
+++ script.js
@@ -21,7 +21,43 @@ const createGameboard = function () {
       : alert(`Sorry, that spot is taken.`);
   };
 
-  return { resetGameboard, getGameboard, assignValue };
+  const evalGameboard = () => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        boardPositions[i][0] === boardPositions[i][1] &&
+        boardPositions[i][1] === boardPositions[i][2]
+      ) {
+        console.log("Winner!");
+        // handleWinner
+      }
+
+      if (
+        boardPositions[0][i] === boardPositions[1][i] &&
+        boardPositions[1][i] === boardPositions[2][i]
+      ) {
+        console.log("Winner!");
+        // handleWinner
+      }
+    }
+
+    if (
+      boardPositions[0][0] === boardPositions[1][1] &&
+      boardPositions[1][1] === boardPositions[2][2]
+    ) {
+      console.log("Winner!");
+      // handleWinner
+    }
+
+    if (
+      boardPositions[0][2] === boardPositions[1][1] &&
+      boardPositions[1][1] === boardPositions[2][0]
+    ) {
+      console.log("Winner!");
+      // handleWinner
+    }
+  };
+
+  return { resetGameboard, getGameboard, assignValue, evalGameboard };
 };
 
 const createPlayer = function () {};
@@ -33,5 +69,11 @@ const gameboard = createGameboard();
 
 gameboard.resetGameboard();
 gameboard.assignValue("x", 1, 1);
-gameboard.assignValue("o", 1, 1);
+gameboard.assignValue("o", 0, 1);
+gameboard.assignValue("x", 1, 2);
+gameboard.assignValue("o", 0, 0);
+gameboard.assignValue("x", 2, 1);
+gameboard.evalGameboard();
+// gameboard.assignValue("o", 0, 2);
+// gameboard.evalGameboard();
 console.log(gameboard.getGameboard());
